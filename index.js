fe008e19c68e89017bbfa3409d84633714e7e7e4
akircher <adam.kircher@gmail.com>
2017-03-15 14:32:33 -0400

    Update to use Kojo csv file

--- index.js
+++ index.js
@@ -1,104 +1,89 @@
+
+
 function flat2nested(obj) {
-  var res = {}
+    var res = {}
+    for (var path in obj) {
+        arr = path.split('.')
+        curr = res
+        for (var i in arr) {
+            var key = arr[i]
+            if (i < arr.length - 1) {
+                curr = curr[key] = curr[key] || {}; continue
+            }
+            if (typeof obj[path] == 'string')
+                obj[path] = obj[path].trim()
 
-  for (var path in obj) {
-    arr  = path.split('.')
-    curr = res
-    for (var i in arr) {
-      var key = arr[i]
-      if (i < arr.length-1) {
-        curr = curr[key] = curr[key] || {}; continue
-      }
-
-      if (typeof obj[path] == 'string')
-        obj[path] = obj[path].trim()
-
-      curr[key] = obj[path] || null
+            curr[key] = obj[path] || null
+        }
     }
-  }
-
-  return res
+    return res
 }
 
-var nested2flat = function(obj) {
+var nested2flat = function (obj) {
+    var flat = {}
+    for (var i in obj) {
+        if (obj[i] === null || typeof obj[i] != 'object') {
+            flat[i] = obj[i]; continue
+        }
+        var flatObject = nested2flat(obj[i])
+        var delimited = i && !Array.isArray(obj)
+        for (var j in flatObject) {
 
-	var flat = {}
+            key = delimited ? i + '.' + j : j
 
-	for (var i in obj) {
-
-		if (obj[i] === null || typeof obj[i] != 'object') {
-      flat[i]  = obj[i]; continue
+            flat[key]
+                ? flat[key] += ';' + flatObject[j]
+                : flat[key] = flatObject[j]
+        }
     }
-
-		var flatObject = nested2flat(obj[i])
-    var delimited  = i && ! Array.isArray(obj)
-		for (var j in flatObject) {
-
-      key = delimited ? i + '.' + j : j
-
-			flat[key]
-        ? flat[key] += ';'+flatObject[j]
-        : flat[key]  = flatObject[j]
-    }
-	}
-
-  return flat
+    return flat
 }
 
 function parse(file, rows) {
-  return new Promise((resolve, reject) => {
-    Papa.parse(file, {
-      header:true,
-      dynamicTyping:true,
-      preview:rows,
-      complete:function(results, file) {
-        resolve(results)
-      }
+    return new Promise((resolve, reject) => {
+        Papa.parse(file, {
+            header: true,
+            dynamicTyping: true,
+            preview: rows,
+            complete: function (results, file) {
+                resolve(results)
+            },
+            error: function (err, file) {
+                console.log("error encountered when parsing", err);
+            }
+        })
     })
-  })
 }
 
-window.Csv = function(requiredFields, optionalFields) {
-  this.requiredFields = requiredFields
-  this.optionalFields = optionalFields
+function toJSON(file, callback) {
+    return parse(file, 0).then(results => {
+        return results;
+    }).then(results => {
+        for (var i in results.data) {
+            results.data[i] = flat2nested(results.data[i])
+        }
+        var final = results.data.reverse()
+        return callback(final); 
+    }).then(dbresults => {
+        let file = new Blob([dbresults], { type: 'text/csv;charset=utf-8;' })
+        let link = document.createElement('a')
+        link.href = window.URL.createObjectURL(file)
+        link.setAttribute('download', name)
+        link.click()
+    })
 }
 
-Csv.prototype.parse = function(file) {
-  return parse(file, 1).then(results => {
-    var fields = this.requiredFields.concat(this.optionalFields)
-    for (var i in fields)
-      if (! ~ results.meta.fields.indexOf(fields[i]))
-        throw `Missing field ${fields[i]}. CSV must contain the following headers ${fields}`
-  }).then(_ => {
-    return parse(file)
-  }).then(results => {
-    for (var i in results.data) {
-      for (var j in this.requiredFields) {
-        if ( ! results.data[i][this.requiredFields[j]])
-          throw 'CSV missing require value '+this.requiredFields[j]+' on line '+i
-      }
 
-      results.data[i] = flat2nested(results.data[i])
-    }
-    return results.data.reverse()
-  })
-}
-
-Csv.prototype.unparse = function(name, arr) {
-  //Flatten object
-  //TODO some sort of check for all required and optional fields
-  let flat = Papa.unparse(arr.map(row => {
-    //Alphabetically order keys
-    var unsorted = nested2flat(row)
-    var sorted   = {}
-    var fields   = Object.keys(unsorted).concat(this.requiredFields).concat(this.optionalFields)
-    fields.sort().forEach(key => sorted[key] = unsorted[key])
-    return sorted
-  }))
-  let file = new Blob([flat], {type:'text/csv;charset=utf-8;'})
-  let link = document.createElement('a')
-
-  link.href = window.URL.createObjectURL(file)
-  link.setAttribute('download', name)
-  link.click()
+function toCSV(name, arr) {
+    let flat = Papa.unparse(arr.map(row => {
+        var unsorted = nested2flat(row)
+        var sorted = {}
+        Object.keys(unsorted).sort().forEach(key => sorted[key] = unsorted[key])
+        return sorted
+    }))
+    let file = new Blob([dbresults], { type: 'text/csv;charset=utf-8;' })
+    let link = document.createElement('a')
+    link.href = window.URL.createObjectURL(file)
+    link.setAttribute('download', name)
+    link.click()
 }
